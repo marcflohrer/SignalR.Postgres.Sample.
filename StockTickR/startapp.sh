67fe7a6b30242468f32627e55bd4e8f5fd2eca1b
Marc Lohrer <Marc.Lohrer@gmx.de>
2018-08-19 19:02:18 +0200

    remove image prune command to speed up build and run cycle

--- StockTickR/startapp.sh
+++ StockTickR/startapp.sh
@@ -5,5 +5,4 @@ rm -rf ./StockTickR/obj/ \
    && rm -rf ./CsharpClient/bin/ \
    && docker container prune -f \
    && docker-compose up -d  \
-   && docker image prune -f \
    && docker logs -f stocktickr_stocktickr_1 
--- StockTickR/stopapp.sh
+++ StockTickR/stopapp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 docker stop $(docker ps -a -q) \
    && docker container prune -f  \
-   && docker image prune -f \
    && docker network prune -f \
    && docker volume prune -f
   
