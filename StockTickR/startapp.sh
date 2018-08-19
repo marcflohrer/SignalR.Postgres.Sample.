3cf086a3343f276be3b9c1aa5c50fd293dc167eb
Marc Lohrer <Marc.Lohrer@gmx.de>
2018-08-19 09:39:54 +0200

    fix missing trailing slash

--- StockTickR/startapp.sh
+++ StockTickR/startapp.sh
@@ -2,7 +2,7 @@
 rm -rf ./StockTickR/obj/ \
    && rm -rf ./StockTickR/bin/ \
    && rm -rf ./CsharpClient/obj/ \
-   && rm -rf ./CsharpClient/bin/
+   && rm -rf ./CsharpClient/bin/ \
    && docker container prune -f \
    && docker-compose up -d  \
    && docker image prune -f \
