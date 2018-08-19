35db1a380d233b7976c82cb95f9bb5e1574a01ed
Marc Lohrer <Marc.Lohrer@gmx.de>
2018-08-19 19:08:22 +0200

    Add db seed in Program.cs

--- StockTickR/StockTickRApp/Program.cs
+++ StockTickR/StockTickRApp/Program.cs
@@ -3,6 +3,10 @@
 
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StockTickR.Repositories;
+using StockTickR.Repositories.Core;
 
 namespace StockTickR
 {
@@ -10,12 +14,31 @@ namespace StockTickR
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build()
+                                                 .Seed();
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseUrls("http://0.0.0.0:8081");
+    }
+
+    public static class WebHostExtension
+    {
+        public static IWebHost Seed(this IWebHost webhost)
+        {
+            // alternatively resolve UserManager instead and pass that if only think you want to seed are the users     
+            var stockDbContext = webhost.Services.GetService<IServiceScopeFactory>()
+                                        .CreateScope()
+                                        .ServiceProvider
+                                        .GetRequiredService<StockDbContext>();
+            if (!stockDbContext.AllMigrationsApplied())
+            {
+                stockDbContext.Database.Migrate();
+            }
+            return webhost;
+        }
     }
 }
